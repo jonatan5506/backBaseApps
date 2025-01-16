7de8e8f570923e25b5c2292aab875883f991e3bb
Jonatan_AndradeL <120528971+jonatan5506@users.noreply.github.com>
2025-01-15 21:58:19 -0300

    user medico

---
+++ src/colaborador/medico/colaboradorMedico.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'usuario_medico' })
+export class ColaboradorMedicoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ name: 'nome', length: 100, nullable: false })
+  nome: string;
+  @Column({ name: 'cpf', length: 20, nullable: false })
+  cpf: string;
+  @Column({ name: 'crm', length: 20, nullable: false })
+  crm: string;
+  @Column({ name: 'celular', length: 25 })
+  celular: string;
+  @Column({ name: 'email', length: 50, nullable: false })
+  email: string;
+  @Column({ name: 'data_nascimento', length: 20, nullable: false })
+  dataDeNascimento: Date;
+  @Column({ name: 'imagem', length: 150 })
+  imagem: string;
+
+  //Auditar entidade
+  @CreateDateColumn({ name: 'created_at' })
+  createAt: string;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updateAt: string;
+  @DeleteDateColumn({ name: 'delete_at' })
+  deleteAt: string;
+}
